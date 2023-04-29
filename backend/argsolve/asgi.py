349ffc8d393d60e30377dcc9ca9d4f0adfd60203
Jake Dickie <crjake@gmail.com>
2023-04-29 20:39:06 +0100

    Attempted to fix bug for deployment

--- backend/argsolve/asgi.py
+++ backend/argsolve/asgi.py
@@ -6,13 +6,15 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'argsolve.settings')
 
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 from api.routing import websocket_urlpatterns
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import os
 
 import sys
 sys.path.append('..')
